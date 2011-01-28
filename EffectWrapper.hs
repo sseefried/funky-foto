0268137deb2132e54b93f73e831ebaeba92d225a
Ben Lever <ben.lever@nicta.com.au>
2011-01-28 16:41:22 +1100

    Wibbles - refactor effect code wrapper.

--- EffectWrapper.hs
+++ EffectWrapper.hs
@@ -14,37 +14,42 @@ import System.IO
 import Control.Monad
 
 
--- | The effect program: ./prog in-bitmap-file out-bitmap-file
---
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- The effect program: ./prog in-jpg-file out-jpg-file
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 main = do
   [inFile, outFile] <- getArgs
 
-  bmpIn  <- liftM (either (error . show) id) $ readBMP inFile
-  arrIn  <- bmpToArray bmpIn
+  arrIn  <- bmpToArray inFile
+  let arrOut = job arrIn
+  arrayToBmp outFile arrOut
 
-  bmpOut <- arrayToBmp (job arrIn)
-  h      <- openFile outFile WriteMode
-  hPutBMP h bmpOut
-  hClose h
 
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Image I/O
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 -- | Convert bitmap value to 3D accelerate array.
 --
-bmpToArray :: BMP -> IO (Array DIM3 Word8)
-bmpToArray bmp = byteStringsToArray dim ((), unpackBMPToRGBA32 bmp)
-  where
-    (w, h) = bmpDimensions bmp
-    dim    = (Z :. h :. w :. 4)
+bmpToArray :: FilePath -> IO (Array DIM3 Word8)
+bmpToArray bmpFile = do
+  bmp  <- liftM (either (error . show) id) $ readBMP bmpFile
+  let (w, h) = bmpDimensions bmp
+      dim    = (Z :. h :. w :. 4)
+  byteStringsToArray dim ((), unpackBMPToRGBA32 bmp)
+
 
 -- | Convert 3D acclerate array to a bitmap value.
 --
-arrayToBmp :: Array DIM3 Word8 -> IO BMP
-arrayToBmp arr = do
-  ((),bs) <- arrayToByteStrings arr
-  return $ packRGBA32ToBMP w h bs
+arrayToBmp :: FilePath -> Array DIM3 Word8 -> IO ()
+arrayToBmp bmpFile arr = do
+  ((), bs) <- arrayToByteStrings arr
+  let bmp = packRGBA32ToBMP w h bs
+  h <- openFile bmpFile WriteMode
+  hPutBMP h bmp
+  hClose h
   where
     (Z :. h :. w :. _) = arrayShape arr
-
 
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -55,4 +60,3 @@ job :: Array DIM3 Word8 -> Array DIM3 Word8
 job arr = CUDA.run $ effect $ use arr
 
 effect :: Acc (Array DIM3 Word8) -> Acc (Array DIM3 Word8)
-
