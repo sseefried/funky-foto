335ee667ecd8b4750a46fa5595833dd5082cfa96
Ben Lever <ben.lever@nicta.com.au>
2011-02-07 09:55:47 +1100

    Modify 'effect' function to work on Int arrays.

--- EffectWrapper.hs
+++ EffectWrapper.hs
@@ -57,10 +57,10 @@ arrayToBmp bmpFile arr = do
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 job :: Array DIM3 Word8 -> Array DIM3 Word8
-job arr = CUDA.run $ effect $ use arr
+job arr = CUDA.run $ Acc.map (intToWord8) $ effect $ Acc.map (word8ToInt) $ use arr
   where
     -- Useful values to bring into scope
     arrDim       = arrayShape arr
     (Z:.h:.w:.d) = arrDim
 
-    effect :: Acc (Array DIM3 Word8) -> Acc (Array DIM3 Word8)
+    effect :: Acc (Array DIM3 Int) -> Acc (Array DIM3 Int)
